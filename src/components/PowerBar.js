575d99cf0adc4532f54f830d9333ec96f869f4af
Nur Fahmi <rfahmi@outlook.com>
2023-01-05 01:31:26 +0800

    fix assets

--- src/components/PowerBar.js
+++ src/components/PowerBar.js
@@ -1,20 +1,25 @@
 import React, {memo} from 'react';
+import { View } from 'react-native';
 import * as Progress from 'react-native-progress';
 
 const PowerBar = ({width, current, max}) => {
   const done = 1 - current / max;
   if (done < 0) {
-    // console.log('DONE IS INVALID');
+    console.log('DONE IS INVALID');
+    return (
+      <View />
+    )
+  }else{
+    return (
+      <Progress.Bar
+        animated={false}
+        progress={100}
+        width={width}
+        color={done < 1 ? 'orange' : 'gray'}
+        unfilledColor="#eee"
+      />
+    );
   }
-  return (
-    <Progress.Bar
-      animated={false}
-      progress={done}
-      width={width}
-      color={done < 1 ? 'orange' : 'gray'}
-      unfilledColor="#eee"
-    />
-  );
 };
 
 export default memo(PowerBar);
