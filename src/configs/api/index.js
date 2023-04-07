39804cf22985d911e3af397565da8d3e1a4d6ea3
Nur Fahmi <hello@rfahmi.com>
2023-04-08 00:33:41 +0700

    add dymanic versioning & new npm script

--- src/configs/api/index.js
+++ src/configs/api/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import debounce from 'debounce';
+import {getVersion, getBuildNumber} from 'react-native-version';
 
-export const app_version = 168;
-export const app_version_name = '3.6.8';
+export const app_version = getBuildNumber();
+export const app_version_name = getVersion();
 export let baseURL = __DEV__
   ? 'https://api3.harnic.id/v3'
   : // 'http://10.0.2.2:8000/v3'
