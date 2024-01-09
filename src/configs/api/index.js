0f690c81be0d74da43b02a58e10bbc3b03d0445d
Nur Muhamad Rizalul Fahmi <nur.fahmi@prudential.co.id>
2024-01-09 16:19:48 +0700

    sync

--- src/configs/api/index.js
+++ src/configs/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import debounce from 'debounce';
 
-export const app_version = 198;
-export const app_version_name = '3.9.3';
+export const app_version = 199;
+export const app_version_name = '3.9.4';
 export let baseURL = __DEV__
   ? 'https://api3.harnic.id/v3'
   : // 'http://10.0.2.2:8000/v3'
