554bbb9c69b353054387c1f832b4566d743f5d0f
Nur Fahmi <hello@rfahmi.com>
2024-04-28 21:37:48 +0700

    sync

--- src/configs/api/index.js
+++ src/configs/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import debounce from 'debounce';
 
-export const app_version = 205;
-export const app_version_name = '3.10.1';
+export const app_version = 206;
+export const app_version_name = '3.10.2';
 export let baseURL = __DEV__
   ? 'https://api3.harnic.id/v3'
   : // 'http://10.0.2.2:8000/v3'
